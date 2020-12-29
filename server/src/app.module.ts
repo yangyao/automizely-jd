4b343cc5e8c58caebb068e7e1459a819daa82206
Yao Yang <y.yang@aftership.com>
2020-12-29 13:38:57 +0800

    sep client and server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -8,7 +8,7 @@ import { join } from 'path';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'client/build'),
+      rootPath: join(__dirname, '../../', 'client/build'),
     }),
     JdModule
   ],
